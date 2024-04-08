8c7fc3c8943b92403c30d876815c88713d4ce011
fujitakeiei <masakazu777@ymail.ne.jp>
2024-04-08 12:37:37 +0000

    delete method

--- babel.config.js
+++ babel.config.js
@@ -54,13 +54,13 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-prorosal-private-methods',
+        '@babel/plugin-transform-private-methods',
         {
           loose: true
         }
       ],
       [
-        '@babel/plugin-prorosal-private-property-in-object',
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
